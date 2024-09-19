dbc4646629fc30e52f8a38cf523a59c28fe85823
DNANO <dnano@stclaircollege.ca>
2024-09-19 17:28:49 -0400

    creating student-detail component

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import {User} from "../Shared/Modules/user";
 import {NgForOf} from "@angular/common";
+import {StudentDetailComponent} from "../student-detail/student-detail.component";
 
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, StudentDetailComponent],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.scss'
 })
 export class StudentListComponent {
 
-  displayedColumns:string[]= ['id', 'firstName', 'lastName', 'department', 'isAdmin'];S
+  displayedColumns:string[]= ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
   userList: User[] = [ //Copied from app.component.ts
     {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
     {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
