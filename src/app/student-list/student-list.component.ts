dd9aa20aae30bf01e7f5639c2552e88290c61b8f
DNANO <dnano@stclaircollege.ca>
2024-09-19 17:33:53 -0400

    creating student-detail component

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -13,12 +13,18 @@ import {StudentDetailComponent} from "../student-detail/student-detail.component
 })
 export class StudentListComponent {
 
-  displayedColumns:string[]= ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
   userList: User[] = [ //Copied from app.component.ts
     {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
     {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
     {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true}
+    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
   ];
+  //Catch the onclick event from the html
+  selectedStudent?: User;
 
+  //function to set which student to display
+  selectStudent(student: User): void {
+    this.selectedStudent = student;
+  }
 }
