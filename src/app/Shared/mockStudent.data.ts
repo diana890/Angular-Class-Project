695cf569ddd6b72b7e0b006a48ab54438d33aab3
DNANO <dnano@stclaircollege.ca>
2024-09-26 17:38:15 -0400

    creating service

---
+++ src/app/Shared/mockStudent.data.ts
@@ -0,0 +1,9 @@
+import {User} from "./Modules/user";
+
+
+export const userList: User[] = [ //Copied from app.component.ts
+  {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
+  {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
+  {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
+  {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
+];
--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -14,12 +14,11 @@ import {StudentDetailComponent} from "../student-detail/student-detail.component
 export class StudentListComponent {
 
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
-  userList: User[] = [ //Copied from app.component.ts
-    {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
-    {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
-    {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
-  ];
+  userList:User[]=[];
+
+
+
+
   //Catch the onclick event from the html
   selectedStudent?: User;
 
