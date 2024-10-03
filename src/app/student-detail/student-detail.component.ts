06e136e04d1014546f5acdbbd5e396818e6e06ad
DNANO <dnano@stclaircollege.ca>
2024-10-03 17:39:22 -0400

    set the routing

--- src/app/student-detail/student-detail.component.ts
+++ src/app/student-detail/student-detail.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../Shared/Modules/user";
 import {NgIf} from "@angular/common";
+import {ActivatedRoute} from "@angular/router";
+import {StudentService} from "../Services/student.service";
 
 
 @Component({
@@ -10,8 +12,37 @@ import {NgIf} from "@angular/common";
   templateUrl: './student-detail.component.html',
   styleUrl: './student-detail.component.scss'
 })
-export class StudentDetailComponent {
+export class StudentDetailComponent implements OnInit{
+
+  student: User | undefined;
+  userList: User[] = [];
+  currentIndex: number = 0;
+
+  constructor(
+    private route: ActivatedRoute,
+    private studentService: StudentService
+
+  ) {}
 
 
-  @Input() student?:User;
+    ngOnInit(): void {
+      this.studentService.getStudents().subscribe(users => {
+        this.userList = users;
+
+        // Subscribe to paramMap changes to actually see the page changing
+        //If we dont do this, the URL will change but the view will not
+        this.route.paramMap.subscribe(params => {
+          const id = Number(params.get('id'));
+          if (id) {
+            this.currentIndex = this.userList.findIndex(user => user.id === id);
+            this.student = this.userList[this.currentIndex];
+          }
+        });
+      });
+    }
+
+
+
+
+
 }
--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -3,12 +3,13 @@ import {User} from "../Shared/Modules/user";
 import {NgForOf} from "@angular/common";
 import {StudentDetailComponent} from "../student-detail/student-detail.component";
 import {StudentService} from "../Services/student.service";
+import {RouterLink} from "@angular/router";
 
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [NgForOf, StudentDetailComponent],
+  imports: [NgForOf, StudentDetailComponent, RouterLink],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.scss'
 })
