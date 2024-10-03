c472b2085a317c9da824f60920f9532851adb3d9
DNANO <dnano@stclaircollege.ca>
2024-10-03 16:54:19 -0400

    set the routing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {StudentListComponent} from "./app/student-list/student-list.component";
+import {StudentDetailComponent} from "./app/student-detail/student-detail.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+
+
+
+const routes:Routes= [
+
+  {path: 'students', component: StudentListComponent},
+  {path:'students/:id', component: StudentDetailComponent}
+]
+
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+})
+
